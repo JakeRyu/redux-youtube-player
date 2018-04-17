4af9c0d450e8fa43d8531dc5d10e38713d8bb2e4
Jake Ryu <jake.ryu@hotmail.co.uk>
2018-04-17 19:36:41 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBe4F0kE13Dngw8RQ95vneuaXurbcw3omo';
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(
+    <App />
+  , document.querySelector('.container'));
