417c724ba722cce1cf66529a5654e792f301f402
Jake Ryu <jake.ryu@hotmail.co.uk>
2018-04-17 23:17:24 +0100

    Controlled component by adding value property associated with state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,22 +1,21 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };  // inside constructor we can set an object to state
+    this.state = {
+      term: 'Search'
+    }; // inside constructor we can set an object to state
     // everywhere else we should use this.setState({object})
   }
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
-        <p>Value of the input: {this.state.term}</p>
-      </div>
-
-    ) ;
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>
+    </div>);
   }
 }
 
